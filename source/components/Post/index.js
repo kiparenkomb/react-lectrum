980fb37c37d6056d07479d714741d71bc17c6c8c
nkiparenko <nkiparenko@corp.web4pro.com.ua>
2018-06-01 22:55:24 +0300

    Added module-styles for components

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,14 +1,17 @@
+// Post.js
+
 // Core
 import React, { Component } from 'react';
 import moment from 'moment';
 
 // Instruments
 import avatar from 'theme/assets/lisa';
+import Styles from './styles.m.css';
 
 export default class Post extends Component {
     render () {
         return (
-            <section>
+            <section className = { Styles.post }>
                 <img src = { avatar } />
                 <a>Lisa Simpson</a>
                 <time>{moment().format('MMMM D h:mm:ss a')}</time>
---
+++ source/components/StatusBar/index.js
@@ -0,0 +1,23 @@
+// StatusBar.js
+
+// Core
+import React, { Component } from 'react';
+
+// Instruments
+import avatar from 'theme/assets/lisa';
+import Styles from './styles.m.css';
+
+export default class StatusBar extends Component {
+    render () {
+        return (
+            <section className = { Styles.statusBar }>
+                <button>
+                    <img src = { avatar } />
+                    <span>Lisa</span>
+                    &nbsp;
+                    <span>Simpson</span>
+                </button>
+            </section>
+        );
+    }
+}
