2ad73f6eb01d6a3091e35523a5066e077f2b8b5b
nkiparenko <nkiparenko@corp.web4pro.com.ua>
2018-06-04 12:25:06 +0300

    Added state for Feed component

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -7,17 +7,32 @@ import React, { Component } from 'react';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
+import Spinner from 'components/Spinner';
 
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            { id: '4rg3', comment: 'Hi there!', created: 1526825076849 },
+            { id: '3ek3', comment: 'Hello! 👋', created: 1526825076949 }
+        ],
+        isPostFetching: false,
+    };
     render () {
+        const { posts, isPostFetching } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return <Post key = { post.id } { ...post } />;
+        });
+
         return (
             <section className = { Styles.feed }>
+                <Spinner isSpinning = { isPostFetching } />
                 <StatusBar />
                 <Composer />
-                <Post />
+                {postsJSX}
             </section>
         );
     }
