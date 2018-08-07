88c2e8a10a650594c3cb8781c0b4d18f1c23dfb3
nkiparenko <nkiparenko@corp.web4pro.com.ua>
2018-08-07 17:33:51 +0300

    Added user get cross post

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -30,6 +30,14 @@ export default class Post extends Component {
         _removePost(id);
     };
 
+    _getCross = () => {
+        const { firstName, lastName, currentUserFirstName, currentUserLastName } = this.props;
+
+        return `${firstName} ${lastName}` === `${currentUserFirstName} ${currentUserLastName}`
+            ? (<span className = { Styles.cross } onClick = { this._removePost } />)
+            : null;
+    };
+
     render () {
         const {
             avatar,
@@ -42,12 +50,11 @@ export default class Post extends Component {
             likes,
         } = this.props;
 
+        const cross = this._getCross();
+
         return (
             <section className = { Styles.post }>
-                <span
-                    className = { Styles.cross }
-                    onClick = { this._removePost }
-                />
+                {cross}
                 <img src = { avatar } />
                 <a>{`${firstName} ${lastName}`}</a>
                 <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
