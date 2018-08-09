45c201e947764aab8d0ee15e48a7cf6a04f3778e
nkiparenko <nkiparenko@corp.web4pro.com.ua>
2018-08-09 17:57:05 +0300

    Added web socket

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -13,7 +13,9 @@ import Spinner from 'components/Spinner';
 
 // Instruments
 import Styles from './styles.m.css';
-import { api, TOKEN } from 'config/api';
+import { api, TOKEN, GROUP_ID } from 'config/api';
+//import { socket } from '../../socket/init';
+import { socket } from 'socket/init';
 
 @withProfile
 export default class Feed extends Component {
@@ -23,12 +25,58 @@ export default class Feed extends Component {
     };
 
     componentDidMount () {
+        const { currentUserFirstName, currentUserLastName } = this.props;
+
         this._fetchPosts();
-        this.refetch = setInterval(this._fetchPosts, 1000);
+
+        socket.emit('join', GROUP_ID);
+
+        socket.on('create', (postJSON) => {
+            const { data: createdPost, meta } = JSON.parse(postJSON);
+
+            if (
+                `${currentUserFirstName} ${currentUserLastName}` !==
+                `${meta.authorFirstName} ${meta.authorLastName}`
+            ) {
+                this.setState(({ posts }) => ({
+                    posts: [createdPost, ...posts],
+                }));
+            }
+        });
+
+        socket.on('remove', (postJSON) => {
+            const { data: removedPost, meta } = JSON.parse(postJSON);
+
+            if (
+                `${currentUserFirstName} ${currentUserLastName}` !==
+                `${meta.authorFirstName} ${meta.authorLastName}`
+            ) {
+                this.setState(({ posts }) => ({
+                    posts: posts.filter((post) => post.id !== removedPost.id),
+                }));
+            }
+        });
+
+        socket.on('likes', (postJSON) => {
+            const { data: likedPost, meta } = JSON.parse(postJSON);
+
+            if (
+                `${currentUserFirstName} ${currentUserLastName}` !==
+                `${meta.authorFirstName} ${meta.authorLastName}`
+            ) {
+                this.setState(({ posts }) => ({
+                    posts: posts.map(
+                        (post) => post.id === likedPost.id ? likedPost : post,
+                    ),
+                }));
+            }
+        });
     }
 
     componentWillUnmount () {
-        clearInterval(this.refetch);
+        socket.removeListener('create');
+        socket.removeListener('remove');
+        socket.removeListener('likes');
     }
 
     _setPostsFetchingState = (state) => {
